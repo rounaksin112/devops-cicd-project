c7091c8681fed378639e0094b26c08cd5b5aafc6
Rounak <rounaksin112@gmail.com>
2026-09-12 19:48:17 +0000

    Add Ansible deployment configuration

--- application/src/main/java/com/devops/App.java
+++ application/src/main/java/com/devops/App.java
@@ -1,7 +1,36 @@
 package com.devops;
 
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("DevOps CI/CD Application is running!");
+
+    public static void main(String[] args) throws IOException {
+
+        HttpServer server = HttpServer.create(
+                new InetSocketAddress(8081), 0
+        );
+
+        server.createContext("/", exchange -> {
+
+            String response = "DevOps CI/CD Application is running!";
+
+            exchange.sendResponseHeaders(
+                    200,
+                    response.getBytes().length
+            );
+
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(response.getBytes());
+            }
+        });
+
+        server.start();
+
+        System.out.println(
+                "DevOps application started on port 8080"
+        );
     }
 }
